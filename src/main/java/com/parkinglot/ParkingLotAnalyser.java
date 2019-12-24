85725bf856fcedad3699a51da7d9ca6bfaaecc7b
mvinchankar <mvinchankar18@gmail.com>
2019-12-24 17:28:50 +0530

    ADDED : Test to check for empty slot to park the vehicle

---
+++ src/main/java/com/parkinglot/ParkingLotAnalyser.java
@@ -0,0 +1,8 @@
+package com.parkinglot;
+
+public class ParkingLotAnalyser {
+
+    public boolean allocateEmptySlotToParkVehicles(String vehcilesNamePlate) {
+        return true;
+    }
+}
--- src/test/java/com/parkinglot/ParkingLotAnalyserTest.java
+++ src/test/java/com/parkinglot/ParkingLotAnalyserTest.java
@@ -1,5 +1,15 @@
 package com.parkinglot;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ParkingLotAnalyserTest {
 
+    @Test
+    public void givenParkingPlot_OwnerWillCheckForEmptySlotToParkVehicle() {
+        ParkingLotAnalyser parkingLotAnalyser = new ParkingLotAnalyser();
+        boolean plotToParkVehicles = parkingLotAnalyser.
+                allocateEmptySlotToParkVehicles("MH30 AY 3036");
+        Assert.assertTrue(plotToParkVehicles);
+    }
 }
